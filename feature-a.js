f8bd1d081d83842e3ddd6f6cb505426cb8add932
Neto <netod90@gmail.com>
2022-08-04 08:50:23 -0700

    added some changes

--- feature-a.js
+++ feature-a.js
@@ -1,1 +1,21 @@
-console.log('feature-a');
+
+document.addEventListener('DOMContentLoaded', function(){
+    addWavyUnderline(findAllUnorderedListElements());
+    addInnerHTML();
+})
+
+function addWavyUnderline(elements){
+    elements.style.textDecoration = 'underline wavy';
+    
+}
+
+function findAllUnorderedListElements(){
+    const items = document.querySelector('.ulElements');
+    return items;
+}
+
+function addInnerHTML(){
+    const divT = document.querySelector('#textDiv');
+    const text = 'Hello';
+    divT.innerHTML = text;
+}
